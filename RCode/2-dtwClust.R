61d07ff88d1ba6f4c7248651179645aa66c87990
lacoperon <elliot.james.williams@gmail.com>
2017-09-23 19:24:05 -0400

    Updated to clustering engines by engine num

--- RCode/2-dtwClust.R
+++ RCode/2-dtwClust.R
@@ -23,11 +23,17 @@ classB_ship2_GTM$`PORT SHAFT TORQUE` <- NULL
 
 classB_ship2_GTM <- na.omit(classB_ship2_GTM) #huge assumption, maybe should input guess values
 
-B2GTMSample <- sample_n(classB_ship2_GTM, 10000)
+# Partitions into the two different motors
+B2GTM1A <- filter(classB_ship2_GTM, indicator == "GTM1A")
+B2GTM1B <- filter(classB_ship2_GTM, indicator == "GTM1B")
+
+B2GTMSample <- sample_n(classB_ship2_GTM, 2000)
 B2GTMSample$indicator <- NULL
 B2GTMSample$DateTime  <- NULL
 
 B2GTMSample <- na.omit(B2GTMSample)
+
+
 
 b2gtmz <- scale(B2GTMSample)
 # Using DTW with help of lower bounds and PAM centroids
@@ -35,14 +41,19 @@ clusterResult <- tsclust(b2gtmz, k = 5L,
                     distance = "dtw_lb", centroid = "pam", 
                     seed = 3247, trace = TRUE,
                     control = partitional_control(pam.precompute = FALSE),
-                    args = tsclust_args(dist = list(window.size = 20L)))
+                    args = tsclust_args(dist = list(window.size = 5L)))
 
 B2GTMSample$cluster <- clusterResult@cluster
 
-Cluster1 <- filter(B2GTMSample, cluster == 1)
-Cluster2 <- filter(B2GTMSample, cluster == 2)
-Cluster3 <- filter(B2GTMSample, cluster == 3)
-Cluster4 <- filter(B2GTMSample, cluster == 4)
-Cluster5 <- filter(B2GTMSample, cluster == 5)
+C1A <- filter(B2GTMSample, cluster == 1)
+C2A <- filter(B2GTMSample, cluster == 2)
+C3A <- filter(B2GTMSample, cluster == 3)
+C4A <- filter(B2GTMSample, cluster == 4)
+C5A <- filter(B2GTMSample, cluster == 5)
 
+C1B <- filter(B2GTMSample, cluster == 1)
+C2B <- filter(B2GTMSample, cluster == 2)
+C3B <- filter(B2GTMSample, cluster == 3)
+C4B <- filter(B2GTMSample, cluster == 4)
+C5B <- filter(B2GTMSample, cluster == 5)
 
